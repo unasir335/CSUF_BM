081cbc069e3eac97f1f39773000a3d87560f14ac
Tatiana Klimova <tatiana@Tatianas-MacBook-Pro.local>
2024-09-21 14:40:49 -0700

    Updated base page and the navbar.  Started new Users branch for the user management & logins

--- enterpriseApp/enterpriseApp/settings.py
+++ enterpriseApp/enterpriseApp/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'enterpriseApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fbmDB',
+        'USER': 'root',
+        'PASSWORD': 'PA$$w0rd',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
@@ -117,9 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "static/"
+STATICFILES_DIRS = [
+    'entApp/static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
