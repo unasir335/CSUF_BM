d98f03eb70dabe0ca714ab668231b5b620d6178d
ghostintheshell <mendozajesse36@yahoo.com>
2024-10-04 16:50:08 -0700

    updated store html and product detail and modified url.py

--- enterpriseApp/enterpriseApp/urls.py
+++ enterpriseApp/enterpriseApp/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from entApp import views as entApp_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,4 +30,4 @@ urlpatterns = [
     path('store/', include("products.urls")),
     
 
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
