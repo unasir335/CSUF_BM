bb3844933bab5df5d9d625d885810e8438ac97ff
troyc03 <148022362+troyc03@users.noreply.github.com>
2024-10-05 13:52:51 -0700

    Push admin.py (w/ documentation)

--- enterpriseApp/entApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EntappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'entApp'
--- enterpriseApp/entApp/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class CategoryItem(models.Model):
-    title = models.CharField(max_length=200)
-    available = models.BooleanField(default=True)
