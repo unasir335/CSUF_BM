8d0f5d918d4d38349c5b4ab87dd6b370c6478207
Troy Chin <148022362+troyc03@users.noreply.github.com>
2024-10-26 15:07:55 -0700

    Update admin.py

--- enterpriseApp/cart/admin.py
+++ enterpriseApp/cart/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+# Register your models here. (Updated: 10/26)
+
+class OrderHistoryAdmin(admin.ModelAdmin):
+    list_display = ('order_number', 'user', 'order_date', 'total_amount')
+    list_filter = ('user', 'order_date')
+    search_fields = ('order_number', 'user__username')
+    ordering = ('__order_date')
+
+admin.site.register(OrderHistoryAdmin)
