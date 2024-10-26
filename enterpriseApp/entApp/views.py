20b363ef0d207d1e6b1c872bea587099889e633a
Troy Chin <148022362+troyc03@users.noreply.github.com>
2024-10-26 15:01:00 -0700

    Update views.py

--- enterpriseApp/entApp/views.py
+++ enterpriseApp/entApp/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render, HttpResponse
 from .models import CategoryItem
+from django.contrib.auth.forms import AuthenticationForm
 
-# Create your views here.
+# Create your views here. (Updated: 10/26)
 def home(request):
-    return render(request,"home.html")
+    featured_items = CategoryItem.objects.filter(available=True)[:5] # Get first 5 available items
+    return render(request,"home.html") 
 
 def login(request):
-    return render(request,"login.html")
-
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+    if form.is_valid:
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+    if username is not None:
+        login(request, username)
+        return HttpResponse("Login successful!")
+    else:
+        form = AuthenticationForm()
+    return render(request,"login.html", {'form': form})
+         
 def categories(request):
     catItems = CategoryItem.objects.all()
     return render(request, "categories.html", {"categories": catItems})
