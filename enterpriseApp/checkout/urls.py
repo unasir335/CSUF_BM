feb60cba247fdfc204ba38b9523b8b543a98d7e5
UNasir335 <unasir@csu.fullerton.edu>
2024-10-26 21:29:45 -0700

    added README

--- enterpriseApp/checkout/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.checkout, name='checkout'),
-    path('order_complete/<str:order_number>/', views.order_complete, name='order_complete'),  # Make sure this name matches
-    path('track_order/', views.track_order, name='track_order'),
-    path('order_status/<str:order_number>/', views.order_status, name='order_status'),
-]
--- enterpriseApp/entApp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from accounts import views as account_views
-
-urlpatterns = [
-    path("", views.home, name="home"),
-    path("about-us/", views.aboutUs, name="about"),
-
-]
